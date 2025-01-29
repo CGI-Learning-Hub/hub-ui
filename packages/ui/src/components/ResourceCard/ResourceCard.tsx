5c32ea21b864913c65987b302362b85f75a1ee0f
Mattox0 <matbegaaar@gmail.com>
2025-01-29 14:13:42 +0100

    feat(resource-card): add missing props

--- packages/ui/src/components/ResourceCard/ResourceCard.tsx
+++ packages/ui/src/components/ResourceCard/ResourceCard.tsx
@@ -94,6 +94,7 @@ export const ResourceCard = forwardRef<HTMLDivElement, ResourceCardProps>(({
           <ResourceCardImage 
             image={image}
             logo={logo}
+            size={size}
             role="img"
             aria-label={`Image for ${title}`}
           />
@@ -109,6 +110,7 @@ export const ResourceCard = forwardRef<HTMLDivElement, ResourceCardProps>(({
           </EllipsisWithTooltip>
           {propertyItems.length > 0 && propertyItems.map((property, index) => (
             <ResourcePropertyItem
+              size={size}
               key={`property-${index}`}
               role="group"
               aria-label={`Property ${index}`}
@@ -134,6 +136,7 @@ export const ResourceCard = forwardRef<HTMLDivElement, ResourceCardProps>(({
                 key={`info-${index}`}
               >
                 <ResourceIconItem
+                  size={size}
                   role="button"
                   tabIndex={0}
                   aria-label={infoItem.text}
