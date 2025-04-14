67a5eb59e4dd2d4b4c64b915e57d46132bb1bd01
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2025-04-14 17:09:39 +0200

    feat(ui): custom autocomplete

---
+++ packages/ui/src/components/Autocomplete/Autocomplete.tsx
@@ -0,0 +1,89 @@
+import { Divider } from "@mui/material";
+import MuiAutocomplete, { AutocompleteProps as MUIAutocompleteProps } from "@mui/material/Autocomplete";
+import { FC, ReactNode, useEffect, useState } from "react";
+
+export type AutocompleteProps<
+  T,
+  Multiple extends boolean = false,
+  DisableClearable extends boolean = false,
+  FreeSolo extends boolean = false
+> = MUIAutocompleteProps<T, Multiple, DisableClearable, FreeSolo> & {
+    isAllSelectable?: boolean;
+    onSelectAll?: () => void;
+    onDeselectAll?: () => void;
+    selectAllLabel?: ReactNode;
+    deselectAllLabel?: ReactNode;
+};
+
+enum SelectPossibility {
+    SELECT_ALL = "TOUT_SELECTIONNER",
+    DESELECT_ALL = "TOUT_DE_SELECTIONNER",
+}
+
+const Autocomplete: FC<AutocompleteProps<any, boolean, boolean, boolean>> = ({
+    isAllSelectable= false,
+    onSelectAll,        
+    onDeselectAll,
+    selectAllLabel = "Tout sélectionner",
+    deselectAllLabel = "Tout désélectionner",
+    ...otherProps
+}) => {
+
+    if (!isAllSelectable || !otherProps.multiple)
+        return <MuiAutocomplete {...otherProps} />;
+
+    const [showSelectAll, setShowSelectAll] = useState(true);    
+
+    const handleSelectAll = () => {
+        !!onSelectAll && onSelectAll();
+        setShowSelectAll(false);
+    };
+
+    const handleDeselectAll = () => {
+        !!onDeselectAll && onDeselectAll();
+        setShowSelectAll(true);
+    };
+
+    useEffect(() => {
+        setShowSelectAll(otherProps.value.length !== otherProps.options.length);
+    }, [otherProps.value, otherProps.options]);
+
+    const options = [ showSelectAll ? SelectPossibility.SELECT_ALL : SelectPossibility.DESELECT_ALL , ...otherProps.options ];
+   
+    return <MuiAutocomplete {...otherProps}
+    options={options}
+    renderOption={(props, option) => {
+        if(Object.values(SelectPossibility).includes(option as SelectPossibility)) {
+            const selectOption = option as SelectPossibility;
+            
+            return (
+                <>
+                <li {...props}
+                onClick={
+                    selectOption === SelectPossibility.SELECT_ALL
+                        ? handleSelectAll
+                        : handleDeselectAll
+                }
+                >
+                    {
+                        showSelectAll
+                        ? selectAllLabel
+                        : deselectAllLabel
+                    }
+                </li>
+                <Divider sx={{marginTop:1, marginBottom:1}}/></>
+            )
+        }
+        return (
+            <li
+            {...props}
+            >
+            {option}
+            </li>
+        );
+        }
+      }
+    />
+}
+
+export default Autocomplete;
