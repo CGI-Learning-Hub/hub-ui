62b55654c3c9fd90468a2f9a91b3dcfe6b24ac2f
Guillaume MARESCHAL <guillaume.mareschal@cgi.com>
2025-07-30 16:14:33 +0200

    fix(ActionBar): #RDV-84 remove commented code

--- packages/ui/src/components/stories/ActionBar.stories.tsx
+++ packages/ui/src/components/stories/ActionBar.stories.tsx
@@ -3,29 +3,6 @@ import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { ActionBar } from "../ActionBar";
 
-// const meta: Meta<typeof ActionBar> = {
-//   title: "Components/ActionBar",
-//   component: ActionBar,
-// };
-// export default meta;
-
-// type Story = StoryObj<typeof ActionBar>;
-
-// export const Default: Story = {
-//   args: {
-//     leftActions: [
-//       { label: "Ouvrir", action: () => ({}) },
-//       { label: "Fermer", action: () => ({}) },
-//       { label: "Partager", action: () => ({}) },
-//       { label: "AutreBouton", action: () => ({}) },
-//     ],
-//     rightActions: [
-//       { label: "Tout selectionner", action: () => ({}) },
-//       { label: "Tout déselectionner", action: () => ({}) },
-//     ],
-//   },
-// };
-
 const meta: Meta<typeof ActionBar> = {
   title: "Components/ActionBar",
   component: ActionBar,
