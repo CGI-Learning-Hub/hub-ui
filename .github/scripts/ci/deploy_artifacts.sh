fa1e49066030e0c71a1d1c3726b26327fb1fcb0a
Guillaume Roux <guillaume.roux@cgi.com>
2026-09-08 14:27:33 +0200

    chore: edit ci publish script

--- .github/scripts/ci/deploy_artifacts.sh
+++ .github/scripts/ci/deploy_artifacts.sh
@@ -1,16 +1,12 @@
 #/bin/bash
 
-# set registry npmjs
 pnpm config set registry=https://registry.npmjs.org/
 pnpm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
 
-# publish
-# pnpm run publish --access=public
-
 echo "Reading branch $BRANCH_NAME"
 
-if [ "$BRANCH_NAME" = "dev" ]; then
-    pnpm run publish:dev --access=public --tag develop
-else
-    pnpm run publish --access=public
+if [ "$BRANCH_NAME" = "main" ]; then
+    pnpm run publish
+elif [ "$BRANCH_NAME" = "dev" ]; then
+    pnpm run publish:dev
 fi
